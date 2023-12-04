6fbdf86c1c61a098d4bce6a30ab73a91a8379209
Hieu Pham-origin <hieupham112003@gmail.com>
2023-12-04 21:54:24 +0700

    edit error in admin

--- Final/src/main/java/com/example/demo/controller/PassengerController.java
+++ Final/src/main/java/com/example/demo/controller/PassengerController.java
@@ -8,10 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
+@RequestMapping("/Admin")
 public class PassengerController {
     @Autowired
     private PassengerService passengerService;
