c1c1ea771e4a5fa6df1fbd1eee2396fef8bfa076
Tran Huynh Dung <101487156+HDSamO@users.noreply.github.com>
2023-12-04 22:38:22 +0700

    complete controllers

--- Final/src/main/java/com/example/demo/controller/PassengerController.java
+++ Final/src/main/java/com/example/demo/controller/PassengerController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import com.example.demo.entity.Flight;
 import com.example.demo.entity.Passenger;
 import com.example.demo.entity.Plane;
 import com.example.demo.service.PassengerService;
@@ -7,16 +8,18 @@ import com.example.demo.service.PlaneService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
+@RequestMapping("/Admin")
 public class PassengerController {
     @Autowired
     private PassengerService passengerService;
     @GetMapping("/passenger")
-    public String showPlanes(Model model){
+    public String showPassengers(Model model){
         List<Passenger> passengers = passengerService.fetchAllPassenger();
         model.addAttribute("passengerList", passengers);
         return "Admin/passenger-manager";
